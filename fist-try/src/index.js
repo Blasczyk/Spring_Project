23a1ca59cf8e7fde1fd620234c72805fe73783cd
Blasczyk <blasczykawr@gmail.com>
2026-03-30 09:14:24 -0700

    updates to App.js

--- fist-try/src/index.js
+++ fist-try/src/index.js
@@ -1,5 +1,13 @@
 import{StrictMode} from 'react';
-import {creaeRoot } from 'react-dom/client';
+import {createRoot } from 'react-dom/client';
 import './index.css';
 
 import App from './App';
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
